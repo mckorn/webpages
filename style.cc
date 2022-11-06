2df33eb4c0790be9387309852295ddcd702caf81
McKenna Corn <77704171+mkcorn@users.noreply.github.com>
2022-11-05 21:13:02 -0400

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,28 @@
+/*
+      -Name: McKenna Corn
+      -UnityID: mkcorn
+      -Date: 11/6/10
+      -Assignment: Independent Assignment 5
+      -Description: HTML and Styling
+ */
+ 
+p {
+    font-family: arial;
+	font-size: 13px;
+	color: #9084FA;
+	text-align: center;
+}
+ 
+* {
+    background-color: #FCBA03;
+}
+
+.table-edit {
+    border: 3px solid #D2F7CD;
+}
+
+#photo-edit {
+    max-width: 200px ;
+	max-height: 250px ;
+	border: 2px solid #00FF00;
+}
